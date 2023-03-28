c810046817f7f199e17c487c66a8ddddb703ca73
Oskar R <o.rajzner@gmail.com>
2023-03-28 23:42:41 +0200

    Added swagger documentation, stable version

--- src/main/java/com/backend/AppBackendApplication.java
+++ src/main/java/com/backend/AppBackendApplication.java
@@ -1,10 +1,12 @@
 package com.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@OpenAPIDefinition
 @SpringBootApplication
 public class AppBackendApplication {
 
