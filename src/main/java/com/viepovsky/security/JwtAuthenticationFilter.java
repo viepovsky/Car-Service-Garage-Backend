e363bacd559ab54082cc71b919ee866424589093
Oskar R <o.rajzner@gmail.com>
2023-05-08 00:10:56 +0200

    Update SpringBoot 2.7.9 to 3.0.6

--- src/main/java/com/viepovsky/security/JwtAuthenticationFilter.java
+++ src/main/java/com/viepovsky/security/JwtAuthenticationFilter.java
@@ -3,6 +3,10 @@ package com.viepovsky.security;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +19,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
